0079d8c960baedb5832e01293a0eac2a27d86b19
Adrian <adriangarcia8382@gmail.com>
2023-08-09 08:47:13 +0800

    Add function to play a single round of Rock Paper Scissors

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,41 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-console.log(getComputerChoice())
+// Plays a sinlge round of Rock Paper and Scissors
+function playGame() {
+  const computerOptions = ['rock', 'paper', 'scissors'];
+  computerSelection = getComputerChoice();
+  playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return `You both choose ${playerSelection}. It's a tie!`
+  }
+
+  if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      return "You win! Rock beats Scissors."
+    } else if (computerSelection === "paper") {
+      return "You lose! Paper beats Rock."
+    }
+  }
+
+  if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "You win! Paper beats Rock."
+    } else if (computerSelection === "scissors") {
+      return "You lose! Scissors beats Paper."
+    }
+  }
+
+  if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      return "You win! Scissors beats Paper"
+    } else if (computerSelection === "rock") {
+      return "You lose! Rock beats Scissors."
+    }
+  }
+
+  if (!(computerOptions.includes(playerSelection))) {
+    return "Please choose Rock Paper or Scissors."
+  }
+}
