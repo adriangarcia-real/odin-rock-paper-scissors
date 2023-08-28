31beefe8e7b88c1915dc29d8281f9b944562b91d
Adrian <adriangarcia8382@gmail.com>
2023-08-28 11:26:13 +0800

    Fix indentions

--- scripts/script.js
+++ scripts/script.js
@@ -11,16 +11,16 @@ function incrementScore(playerWins) {
 
 function compareScore() {
   if (playerScore === computerScore) {
-    return `You got ${playerScore} point(s). Computer got ${computerScore} point(s). It's a tie!`
+    return `You got ${playerScore} point(s). Computer got ${computerScore} point(s). It's a tie!`;
   } else if (playerScore > computerScore) {
-    return `You got ${playerScore} point(s). Computer got ${computerScore} point(s). You win!`
+    return `You got ${playerScore} point(s). Computer got ${computerScore} point(s). You win!`;
   } else {
-    return `You got ${playerScore} point(s). Computer got ${computerScore} point(s). You lose!`
+    return `You got ${playerScore} point(s). Computer got ${computerScore} point(s). You lose!`;
   }
 }
 
 function getComputerChoice() {
-  const computerOptions = ['rock', 'paper', 'scissors'];
+  const computerOptions = ["rock", "paper", "scissors"];
   const computerChoice = computerOptions[Math.floor(Math.random() * 3)];
   return computerChoice;
 }
@@ -30,44 +30,44 @@ function playGame(playerSelection, computerSelection) {
     case "rock":
       if (computerSelection === "scissors") {
         incrementScore(true);
-        return "You win! Rock beats Scissors."
+        return "You win! Rock beats Scissors.";
       } else if (computerSelection === "paper") {
         incrementScore(false);
-        return "You lose! Paper beats Rock."
+        return "You lose! Paper beats Rock.";
       } else {
-        return `It's a tie! You both chose ${playerSelection}.`
+        return `It's a tie! You both chose ${playerSelection}.`;
       }
     case "paper":
       if (computerSelection === "rock") {
         incrementScore(true);
-        return "You win! Paper beats Rock."
+        return "You win! Paper beats Rock.";
       } else if (computerSelection === "scissors") {
         incrementScore(false);
-        return "You lose! Scissors beats Paper."
+        return "You lose! Scissors beats Paper.";
       } else {
-        return `It's a tie! You both chose ${playerSelection}.`
+        return `It's a tie! You both chose ${playerSelection}.`;
       }
     case "scissors":
       if (computerSelection === "paper") {
         incrementScore(true);
-        return "You win! Scissors beats Paper"
+        return "You win! Scissors beats Paper";
       } else if (computerSelection === "rock") {
         incrementScore(false);
-        return "You lose! Rock beats Scissors."
+        return "You lose! Rock beats Scissors.";
       } else {
-        return `It's a tie! You both chose ${playerSelection}.`
+        return `It's a tie! You both chose ${playerSelection}.`;
       }
   }
 }
+
 function game() {
   // Play 5 rounds of Rock Paper Scissors
   for (let i = 1; i <= 5; i++) {
-    const computerOptions = ['rock', 'paper', 'scissors'];
+    const computerOptions = ["rock", "paper", "scissors"];
     const computerSelection = getComputerChoice();
     const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
 
-
-    if (!(computerOptions.includes(playerSelection))) {
+    if (!computerOptions.includes(playerSelection)) {
       i -= 1;
       console.log("Please choose Rock Paper or Scissors");
     } else {
@@ -75,11 +75,7 @@ function game() {
     }
   }
 
-  return compareScore()
+  return compareScore();
 }
 
-
-
-console.log(game())
-
-
+console.log(game());
